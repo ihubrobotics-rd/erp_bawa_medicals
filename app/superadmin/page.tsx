fc4915314dd51b27021c001ed9bb5a229c08527a
Sachu S Kumar <sachusajith170@gmail.com>
2025-11-15 11:19:45 +0530

    fix:minor issues

--- app/superadmin/page.tsx
+++ app/superadmin/page.tsx
@@ -162,7 +162,7 @@ export default function AdminDashboard() {
             <TabsContent value="settings" className="space-y-6">
               <SystemSettings />
             </TabsContent>
-                                  
+                          
           </Tabs>
         </div>
       </div>
